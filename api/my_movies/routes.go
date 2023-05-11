42b88f93b2bfcc639ddc948210c3972fef62abc2
ashishp911 <ashish.patil35@gmail.com>
2023-05-11 14:10:51 -0500

    moved stuff from databse.go to bizlogic

--- api/my_movies/routes.go
+++ api/my_movies/routes.go
@@ -14,7 +14,6 @@ func Enter_routes() {
 	my_router.HandleFunc("/movies", CreateMovie).Methods("POST")
 	my_router.HandleFunc("/movies/{id}", UpdateMovie).Methods("PUT")
 	my_router.HandleFunc("/movies/{id}", DeleteMovie).Methods("DELETE")
-
 	fmt.Printf("Starting server at port 8000...\n")
 	log.Fatal(http.ListenAndServe(":8000", my_router))
 }
